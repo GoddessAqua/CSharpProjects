72e9e575248b06ee76de8325ce8cb9a3472ff2bf
RedunViktoria <45935753+RedunViktoria@users.noreply.github.com>
2022-04-21 22:12:41 +0300

    SlotMachine
    Little console game about slot machine

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Slots Player = new Slots();
+
+            while (Player.CurrentBalance > 0)
+            {
+                Console.WriteLine($"ваш баланс: {Player.CurrentBalance}");
+                Console.WriteLine("ваша ставка:");
+                int rate = Convert.ToInt32(Console.ReadLine());
+                if (rate <= Player.CurrentBalance)
+                {
+
+                    Player.MakeRate(rate);
+                    Player.Rotate();
+                    Player.CountResult();
+                    Console.WriteLine($"ваш текущий баланс: {Player.CurrentBalance}");
+                    if (Player.CurrentBalance > 0)
+                    {
+                        Console.WriteLine("будете еще играть? если да, введите 1, если нет 0");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        if (a == 0)
+                        {
+                            Player.TotalCount();
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Нельзя ставить больше, чем есть у вас");
+                }
+            }
+        }
+    }
+}
---
+++ SlotMachine/Slots.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace proj_1
+{
+    class Slots
+    {
+        public static int StartBalance;
+        private int _rate;
+        public int[] ScoreBoard;
+        public static Random rnd;
+        public Slots()
+        {
+            StartBalance = 100;
+            CurrentBalance = StartBalance;
+            Rate = 0;
+            ScoreBoard = new int[3];
+            rnd = new Random();
+        }
+        public int CurrentBalance
+        {
+            get;
+            set;
+        }
+        public int Rate
+        {
+            get
+            { return _rate; }
+            set
+            {
+                if (value > CurrentBalance)
+                {
+                    Console.WriteLine("нельзя поставить больше чем у вас есть");
+                }
+                else
+                {
+                    _rate = value;
+                }
+            }
+        }
+        public void Rotate()
+        {
+            for (int i = 0; i < ScoreBoard.Length; i++)
+            {
+                ScoreBoard[i] = rnd.Next(0, 9);
+            }
+            for (int i = 0; i < ScoreBoard.Length; i++)
+            {
+                if (ScoreBoard[i] == 0)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write(ScoreBoard[i] + " ");
+
+                }
+            }
+            Console.WriteLine();
+        }
+        public void MakeRate(int rate)
+        {
+            Rate = rate;
+            CurrentBalance -= Rate;
+
+        }
+        public void CountResult()
+        {
+            if (ScoreBoard[0] == ScoreBoard[1] && ScoreBoard[0] == ScoreBoard[2])
+            {
+                CurrentBalance += Rate * 2;
+            }
+            else if (ScoreBoard[0] == ScoreBoard[1] && ScoreBoard[2] == 0)
+            {
+                CurrentBalance += Rate;
+            }
+            else if (ScoreBoard[0] == ScoreBoard[1] || ScoreBoard[0] == ScoreBoard[2] || ScoreBoard[1] == ScoreBoard[2])
+            {
+                CurrentBalance += Rate / 2;
+            }
+            else
+            {
+                CurrentBalance += 0;
+            }
+        }
+        public void TotalCount()
+        {
+            if (CurrentBalance > StartBalance)
+            {
+                Console.WriteLine("вы выиграли");
+            }
+            else if (CurrentBalance == StartBalance)
+            {
+                Console.WriteLine("вы ничего не получили");
+            }
+            else
+            {
+                Console.WriteLine("вы проиграли");
+            }
+        }
+
+    }
+}
